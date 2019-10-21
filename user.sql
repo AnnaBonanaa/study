013a56970bbdf9abd852b9f6b19f5bba5f4c9bae
Anna._.Bonana <51794800+AnnaBonanaa@users.noreply.github.com>
2019-10-21 16:30:37 +0300

    Add files via upload

---
+++ user.sql
@@ -0,0 +1,81 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Сен 30 2019 г., 16:19
+-- Версия сервера: 5.6.41
+-- Версия PHP: 5.6.38
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `user`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `hashpassword`
+--
+
+CREATE TABLE `hashpassword` (
+  `hashpassword` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `hashpassword`
+--
+
+INSERT INTO `hashpassword` (`hashpassword`) VALUES
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+(''),
+(''),
+(''),
+(''),
+(''),
+(''),
+(''),
+(''),
+(''),
+('5209ee8c13f092d9ea066f8ab28cf74c'),
+('54f7aefe416fce5c367b327c249828d8'),
+('c006e0702bb5c0c05e1aed8419811bfdef6301fd33e3046583c9d60c20110b1e');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
